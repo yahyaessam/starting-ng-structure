bafc2d18f9942882777f37b718f483859f8a6d93
Yahia Essam Morsy Aly <Yahia.Aly@andalusia.loc>
2019-03-14 19:12:50 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DummyComponent } from './shared/components/dummy/dummy.component';
+import { DirectiveDirective } from './shared/directives/directive.directive';
+import { PipePipe } from './shared/pipes/pipe.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DummyComponent,
+    DirectiveDirective,
+    PipePipe
   ],
   imports: [
     BrowserModule
--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './components/home/home.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HomeComponent, HomePageComponent],
   imports: [
     CommonModule
   ]
